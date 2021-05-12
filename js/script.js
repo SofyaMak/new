570652ecafdab2d86d58b0d84abfd5a7f6c2b2a9
Sofya <sofya.makaryan@gmail.com>
2021-05-12 22:07:54 +0400

    12.05.21

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ "use strict"
+
+const answers = [];
+
+answers[0] = prompt("как ваше имя?", "имя");
+answers[1] = prompt("как ваша фамилия?", "фамилия");
+answers[2] = prompt("ваше возраст?", "18");
+answers[3] = prompt("ваша профессия?", "");
+console.log(answers);
